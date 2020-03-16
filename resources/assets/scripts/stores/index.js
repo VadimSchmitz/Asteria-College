fd4673b74a1f81cb2ee90e2a818a9e3184ed85b2
Levi Deurloo <levimbg@gmail.com>
2020-03-16 23:25:58 +0100

    Update dependencies

--- resources/assets/scripts/stores/index.js
+++ resources/assets/scripts/stores/index.js
@@ -9,7 +9,6 @@ import Global from "./Global";
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 Vue.use(Vuex);
