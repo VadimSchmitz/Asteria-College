39a12382e9cf616c88fa0fc6cbd4393a7c3c99ab
VadimSchmitz <di3mpi3@gmail.com>
2020-04-30 23:26:26 +0200

    basic fullcalendar setup

--- resources/assets/scripts/template/pages/Calendar.vue
+++ resources/assets/scripts/template/pages/Calendar.vue
@@ -1,77 +1,154 @@
 <template>
-    <section class="col-12">
-
-            <p>Asteria College</p>
-            <button @click="get()" v-bind:disabled="usersLoading || users">
-                <i></i>
-                Gebruikers ophalen
-            </button>
-
-
-        <div class="card card-outline card-info mt-3">
-            <div class="overlay" v-show="usersLoading && users == null">
-                <i class="fas fa-2x fa-sync-alt fa-spin"></i>
-            </div>
-
-
-            <div class="p-0">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Naam</th>
-                        <th scope="col">Emailadres
-                            <div class="card-tools d-inline float-right pr-0 mr-0 pb-0">
-                                <button class="btn btn-tool" data-card-widget="maximize" type="button">
-                                    <i class="fas fa-expand"></i>
-                                </button>
-                                <button class="btn btn-tool" data-card-widget="collapse" type="button">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                                <button @click="users = null" class="btn btn-tool" type="button">
-                                    <i class="fas fa-times"></i>
-                                </button>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form @submit.prevent>
+                    <div class="form-group">
+                        <label for="event_name">Event Name</label>
+                        <input type="text" id="event_name" class="form-control" v-model="newEvent.event_name">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="start_date">Start Date</label>
+                                <input
+                                        type="date"
+                                        id="start_date"
+                                        class="form-control"
+                                        v-model="newEvent.start_date"
+                                >
                             </div>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody v-if="users">
-                    <tr :key="user.id" v-for="user in users">
-                        <th scope="row">{{ user.id }}</th>
-                        <td>{{ user.name }}</td>
-                        <td>{{ user.email }}</td>
-                    </tr>
-                    </tbody>
-                </table>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="end_date">End Date</label>
+                                <input type="date" id="end_date" class="form-control" v-model="newEvent.end_date">
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4" v-if="addingMode">
+                            <button class="btn btn-sm btn-primary" @click="addNewEvent">Save Event</button>
+                        </div>
+                        <template v-else>
+                            <div class="col-md-6 mb-4">
+                                <button class="btn btn-sm btn-success" @click="updateEvent">Update</button>
+                                <button class="btn btn-sm btn-danger" @click="deleteEvent">Delete</button>
+                                <button class="btn btn-sm btn-secondary" @click="addingMode = !addingMode">Cancel</button>
+                            </div>
+                        </template>
+                    </div>
+                </form>
             </div>
-
+            <div class="col-md-8">
+                <Fullcalendar @eventClick="showEvent" :plugins="calendarPlugins" :events="events"/>
+            </div>
         </div>
-    </section>
-
+    </div>
 </template>
 
 <script>
+    import Fullcalendar from "@fullcalendar/vue";
+    import dayGridPlugin from "@fullcalendar/daygrid";
+    import interactionPlugin from "@fullcalendar/interaction";
+    import axios from "axios";
     export default {
-        name: 'Dashboard',
+        components: {
+            Fullcalendar
+        },
         data() {
             return {
-                users: null,
-                usersLoading: false
-            }
+                calendarPlugins: [dayGridPlugin, interactionPlugin],
+                events: "",
+                newEvent: {
+                    event_name: "",
+                    start_date: "",
+                    end_date: ""
+                },
+                addingMode: true,
+                indexToUpdate: ""
+            };
+        },
+        created() {
+            this.getEvents();
         },
         methods: {
-            get() {
-                this.usersLoading = true;
-
-                axios.get(`/users`)
-                    .then(response => {
-                        // Timeout only to show loading screen
-                        setTimeout(() => {
-                            this.usersLoading = false;
-                            this.users = response.data;
-                        }, 3000);
-                    }).catch(error => console.error(error));
+            addNewEvent() {
+                axios
+                    .post("/api/calendar", {
+                        ...this.newEvent
+                    })
+                    .then(data => {
+                        this.getEvents(); // update our list of events
+                        this.resetForm(); // clear newEvent properties (e.g. title, start_date and end_date)
+                    })
+                    .catch(err =>
+                        console.log("Unable to add new event!", err.response.data)
+                    );
+            },
+            showEvent(arg) {
+                this.addingMode = false;
+                const { id, title, start, end } = this.events.find(
+                    event => event.id === +arg.event.id
+                );
+                this.indexToUpdate = id;
+                this.newEvent = {
+                    event_name: title,
+                    start_date: start,
+                    end_date: end
+                };
+            },
+            updateEvent() {
+                axios
+                    .put("/api/calendar/" + this.indexToUpdate, {
+                        ...this.newEvent
+                    })
+                    .then(resp => {
+                        this.resetForm();
+                        this.getEvents();
+                        this.addingMode = !this.addingMode;
+                    })
+                    .catch(err =>
+                        console.log("Unable to update event!", err.response.data)
+                    );
+            },
+            deleteEvent() {
+                axios
+                    .delete("/api/calendar/" + this.indexToUpdate)
+                    .then(resp => {
+                        this.resetForm();
+                        this.getEvents();
+                        this.addingMode = !this.addingMode;
+                    })
+                    .catch(err =>
+                        console.log("Unable to delete event!", err.response.data)
+                    );
+            },
+            getEvents() {
+                axios
+                    .get("/api/calendar")
+                    .then(resp => (this.events = resp.data.data))
+                    .catch(err => console.log(err.response.data));
+            },
+            resetForm() {
+                Object.keys(this.newEvent).forEach(key => {
+                    return (this.newEvent[key] = "");
+                });
+            }
+        },
+        watch: {
+            indexToUpdate() {
+                return this.indexToUpdate;
             }
         }
-    }
+    };
 </script>
+
+<style lang="css">
+    @import "~@fullcalendar/core/main.css";
+    @import "~@fullcalendar/daygrid/main.css";
+    .fc-title {
+        color: #fff;
+    }
+    .fc-title:hover {
+        cursor: pointer;
+    }
+</style>
--- resources/assets/scripts/template/pages/Week-1.vue
+++ resources/assets/scripts/template/pages/Week-1.vue
@@ -1,19 +1,77 @@
 <template>
     <section class="col-12">
-        <div class="callout callout-success">
-            <h1>SSSSSSSSSSSSSS</h1>
-            <p>Voor nu een opzetje routing en componenten toegevoegd</p>
+        <div class="callout callout-info">
+            <p>Asteria College</p>
+            <button @click="get()" class="btn btn-outline-info btn-block" v-bind:disabled="usersLoading || users">
+                <i class="fas fa-sync-alt"></i>
+                Gebruikers ophalen
+            </button>
         </div>
 
-        dit is een test
+        <div class="card card-outline card-info mt-3">
+            <div class="overlay" v-show="usersLoading && users == null">
+                <i class="fas fa-2x fa-sync-alt fa-spin"></i>
+            </div>
+
+
+            <div class="p-0">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Naam</th>
+                        <th scope="col">Emailadres
+                            <div class="card-tools d-inline float-right pr-0 mr-0 pb-0">
+                                <button class="btn btn-tool" data-card-widget="maximize" type="button">
+                                    <i class="fas fa-expand"></i>
+                                </button>
+                                <button class="btn btn-tool" data-card-widget="collapse" type="button">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button @click="users = null" class="btn btn-tool" type="button">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody v-if="users">
+                    <tr :key="user.id" v-for="user in users">
+                        <th scope="row">{{ user.id }}</th>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.email }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </section>
+
 </template>
 
 <script>
     export default {
-        name: 'Example',
+        name: 'Dashboard',
         data() {
-            return {}
+            return {
+                users: null,
+                usersLoading: false
+            }
+        },
+        methods: {
+            get() {
+                this.usersLoading = true;
+
+                axios.get(`/users`)
+                    .then(response => {
+                        // Timeout only to show loading screen
+                        setTimeout(() => {
+                            this.usersLoading = false;
+                            this.users = response.data;
+                        }, 3000);
+                    }).catch(error => console.error(error));
+            }
         }
     }
 </script>
