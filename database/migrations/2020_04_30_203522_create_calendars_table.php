8c020c24f2a15476e7d0b7d4c1122f9ae0ff3332
VadimSchmitz <di3mpi3@gmail.com>
2020-05-11 13:52:48 +0200

    bufix
    made it so you dont have to pass an assignment when making or adding a new event.

--- database/migrations/2020_04_30_203522_create_calendars_table.php
+++ database/migrations/2020_04_30_203522_create_calendars_table.php
@@ -16,7 +16,7 @@ class CreateCalendarsTable extends Migration
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
             $table->string('event_name');
-            $table->string('assignment');
+            $table->string('assignment')->nullable();
             $table->dateTime('start_date');
             $table->dateTime('end_date');
             $table->timestamps();
