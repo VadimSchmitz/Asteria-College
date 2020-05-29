105a160f050066aa246ae69a4e9d71b26bdc6280
MariskaH <55788782+MariskaH@users.noreply.github.com>
2020-05-29 05:36:15 +0200

    Update Options.vue

--- resources/assets/scripts/template/pages/Students/components/Options.vue
+++ resources/assets/scripts/template/pages/Students/components/Options.vue
@@ -1,41 +1,47 @@
 <template>
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Opties</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Opties</h3>
 
-            <div class="card-tools">
-                <button class="btn btn-tool" data-card-widget="collapse" type="button"><i class="fas fa-minus"></i>
-                </button>
-            </div>
-        </div>
-        <div class="card-body p-0" style="display: block;">
-            <ul class="nav nav-pills flex-column">
-                <li class="nav-item active">
-                    <a @click="$parent.toggleCreate" class="nav-link cursor-pointer">
-                        Student aanmaken
-                        <span class="badge bg-success float-right"><i class="far fa-plus"></i></span>
-                    </a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link">
-                        Student wijzigen
-                        <span class="badge bg-danger float-right"><i class="far fa-edit"></i></span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link">
-                        Student verwijderen
-                        <span class="badge bg-danger float-right"><i class="far fa-edit"></i></span>
-                    </a>
-                </li>
-
-            </ul>
-        </div>
+      <div class="card-tools">
+        <button class="btn btn-tool" data-card-widget="collapse" type="button">
+          <i class="fas fa-minus"></i>
+        </button>
+      </div>
     </div>
+    <div class="card-body p-0" style="display: block;">
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item active">
+          <a @click="$parent.toggleCreate" class="nav-link cursor-pointer">
+            Student aanmaken
+            <span class="badge bg-success float-right"
+              ><i class="far fa-plus"></i
+            ></span>
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link">
+            Student wijzigen
+            <span class="badge bg-danger float-right"
+              ><i class="far fa-edit"></i
+            ></span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">
+            Student verwijderen
+            <span class="badge bg-danger float-right"
+              ><i class="fas fa-user-minus"></i
+            ></span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Options'
-    }
+export default {
+  name: "Options",
+};
 </script>
